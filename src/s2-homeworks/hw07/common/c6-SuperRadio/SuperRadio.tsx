d0c9b7aea6faa0fa65117969ccde2b55ce99afd0
dnisaev <dnisaev@yandex.ru>
2024-01-20 22:17:43 +0300

    add HW7 solution

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -36,6 +36,12 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // делают студенты
+        let targetId = e.currentTarget.id;
+        let optionId = +targetId[targetId.length - 1]
+
+        if (onChangeOption) {
+            onChangeOption(optionId)
+        }
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -49,7 +55,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       className={finalRadioClassName}
                       type={'radio'}
                       // name, checked, value делают студенты
-
+                      name={o.value}
+                      checked={value === o.id}
+                      value={o.value}
                       onChange={onChangeCallback}
                       {...restProps}
                   />
